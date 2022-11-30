56e0f05ea1efeb0ac539d8b02586b64333f34748
Yaroslav Fedunkiv <ya.fedunkiv@gmail.com>
2022-11-30 19:03:58 +0200

    lesson 51

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {HeaderComponent} from "./header/header.component";
+import {FormsModule} from "@angular/forms";
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
+import { ResipesDetailComponent } from './recipes/resipes-detail/resipes-detail.component';
+import { ResipesItemComponent } from './recipes/recipes-list/resipes-item/resipes-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipesListComponent,
+    ResipesDetailComponent,
+    ResipesItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/recipes/resipes-detail/resipes-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resipes-detail',
+  templateUrl: './resipes-detail.component.html',
+  styleUrls: ['./resipes-detail.component.css']
+})
+export class ResipesDetailComponent {
+
+}
