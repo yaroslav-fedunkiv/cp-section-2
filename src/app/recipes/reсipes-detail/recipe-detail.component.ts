7ab101cbed20e8dc2531455496563ca86cbeff35
Yaroslav_Fedunkiv <ya.fedunkiv@gmail.com>
2022-12-05 14:39:19 +0200

    lesson 166

--- src/app/recipes/reсipes-detail/recipe-detail.component.ts
+++ src/app/recipes/reсipes-detail/recipe-detail.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Recipe} from "../recipes.model";
 import {RecipeService} from "../recipe.service";
-import {ActivatedRoute, Params} from "@angular/router";
+import {ActivatedRoute, Params, Router} from "@angular/router";
 
 @Component({
   selector: 'app-resipes-detail',
@@ -14,7 +14,8 @@ export class RecipeDetailComponent implements OnInit{
 
 
   constructor(private recipeService: RecipeService,
-              private route: ActivatedRoute) {
+              private route: ActivatedRoute,
+              private router: Router) {
   }
 
   ngOnInit() {
@@ -28,5 +29,10 @@ export class RecipeDetailComponent implements OnInit{
 
   onAddToShoppingList(){
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+  onEditRecipe(){
+    this.router.navigate(['edit'], {relativeTo: this.route});
+    // this.router.navigate(['../', this.id, 'edit'], {relativeTo: this.route}); //<== alternative
   }
 }
