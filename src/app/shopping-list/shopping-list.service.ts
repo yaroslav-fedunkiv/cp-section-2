151814ac489cffe59b189a1b8fb9051c195c09ee
Yaroslav Fedunkiv <ya.fedunkiv@gmail.com>
2022-12-07 09:34:55 +0200

    lesson 221

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -3,6 +3,7 @@ import {Subject} from "rxjs";
 
 export class ShoppingListService{
   ingredientsChange = new Subject<Ingredient[]>();
+  startedEditing = new Subject<number>();
   private ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10)
@@ -12,15 +13,16 @@ export class ShoppingListService{
     return this.ingredients.slice();
   }
 
+  getIngredient(index: number){
+    return this.ingredients[index];
+  }
+
   addIngredient(ingredient: Ingredient){
     this.ingredients.push(ingredient);
     this.ingredientsChange.next(this.ingredients.slice());
   }
 
   addIngredients(ingredients: Ingredient[]){
-    // for (let ingredient of ingredients){
-    //   this.addIngredient(ingredient);
-    // }
     this.ingredients.push(...ingredients);
     this.ingredientsChange.next(this.ingredients.slice());
   }
