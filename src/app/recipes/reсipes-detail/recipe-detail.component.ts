9ff123cfbb015c5ce14c055452bcb02a235b9cb1
Yaroslav Fedunkiv <ya.fedunkiv@gmail.com>
2022-12-07 11:05:21 +0200

    lesson 234

--- src/app/recipes/reсipes-detail/recipe-detail.component.ts
+++ src/app/recipes/reсipes-detail/recipe-detail.component.ts
@@ -36,7 +36,8 @@ export class RecipeDetailComponent implements OnInit{
     // this.router.navigate(['../', this.id, 'edit'], {relativeTo: this.route}); //<== alternative
   }
 
-  onDelete(){
+  onDeleteRecipe(){
     this.recipeService.deleteRecipe(this.id);
+    this.router.navigate(['recipes']);
   }
 }
